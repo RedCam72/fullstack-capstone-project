6429dfceee5e46cf2fe0d9b306258870c139c5ea
Jennifer <maroonhighlander18@gmail.com>
2026-03-14 12:28:37 -0500

    Add input validation and update user route

--- giftlink-backend/routes/authRoutes.js
+++ giftlink-backend/routes/authRoutes.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { body, validationResult } = require('express-validator');
 const connectToDatabase = require('../models/db');
 const router = express.Router();
 const dotenv = require('dotenv');
 const pino = require('pino');  //Import Pino logger
+
+//Task 1: Use the `body`,`validationResult` from `express-validator` for input validation
+const { body, validationResult } = require('express-validator');
 
 //Create a Pino logger instance
 const logger = pino();
@@ -19,8 +21,6 @@ router.post('/register', async (req, res) => {
 	try {
 		//Connect to `giftsdb` in MongoDB through `connectToDatabase` in `db.js`
 		const db = await connectToDatabase();
-
-		//Access MongoDB `users` collection
 		const collection = db.collection("users");
 
 		//Check for existing email
@@ -33,6 +33,7 @@ router.post('/register', async (req, res) => {
 		const salt = await bcryptjs.genSalt(10);
 		const hash = await bcryptjs.hash(req.body.password, salt);
 		const email = req.body.email;
+		console.log('email is',email);
 
 		//Save user details in database
 		const newUser = await collection.insertOne({
@@ -95,6 +96,64 @@ router.post('/login', async (req, res) => {
     } catch (e) {
         logger.error(e);
         return res.status(500).json({ error: 'Internal server error', details: e.message });
+    }
+});
+
+// Update API
+router.put('/update', async (req, res) => {
+	// Task 2: Validate the input using `validationResult` and return approiate message if there is an error.
+	const errors = validationResult(req);
+
+	// Task 3: Check if `email` is present in the header and throw an appropriate error message if not present.
+	if (!errors.isEmpty()) {
+		logger.error('Validation errors in update request', errors.array());
+		return res.status(400).json({ errors: errors.array() });
+	}
+	
+    try {
+        const email = req.headers.email;
+		
+		if (!email) {
+			logger.error('Email not found in the request headers');
+			return res.status(400).json({ error: "Email not found in the request headers" });
+		}
+
+        // Task 4: Connect to MongoDB
+		const db = await connectToDatabase();
+		const collection = db.collection("users");
+		
+        // Task 5: find user credentials in database
+		const existingUser = await collection.findOne({ email });
+		
+        if (!existingUser) {
+            logger.error('User not found');
+            return res.status(404).json({ error: "User not found" });
+        }
+
+		existingUser.firstName = req.body.name;
+        existingUser.updatedAt = new Date();
+
+        // Task 6: update user credentials in database
+		const updatedUser = await collection.findOneAndUpdate(
+			{ email },
+			{ $set: existingUser },
+			{ returnDocument: 'after' }
+		);
+
+        // Task 7: create JWT authentication using secret key from .env file
+		const payload = {
+			user: {
+				id: updatedUser._id.toString(),
+			},
+		};
+		
+		const authtoken = jwt.sign(payload, JWT_SECRET);
+		logger.info('User updated successfully');
+
+        res.json({authtoken});
+    } catch (error) {
+		logger.error(error);
+        return res.status(500).send('Internal server error');
     }
 });
 
